4adfcc373d4209081c3b6ec92a8cdb1ba37c0b9b
R1nge <r1ngegamedev@gmail.com>
2022-04-11 10:37:05 +0300

    Added Helper

---
+++ Assets/Scripts/Helper.cs
@@ -0,0 +1,26 @@
+using System;
+
+public static class Helper
+{
+    public static string FormatNumber(float num)
+    {
+        // Ensure number has max 3 significant digits (no rounding up can happen)
+        float i = (float)Math.Pow(10, (int)Math.Max(0, Math.Log10(num) - 2));
+        num = num / i * i;
+        
+        if(num >= 1000000000000000000)
+            return (num / 1000000000000000000D).ToString("0.##") + "Quint";
+        if (num >= 1000000000000000)
+            return (num / 1000000000000000D).ToString("0.##") + "Quad";
+        if (num >= 1000000000000)
+            return (num / 1000000000000D).ToString("0.##") + "T"; 
+        if (num >= 1000000000)
+            return (num / 1000000000D).ToString("0.##") + "B";
+        if (num >= 1000000)
+            return (num / 1000000D).ToString("0.##") + "M";
+        if (num >= 1000)
+            return (num / 1000D).ToString("0.##") + "K";
+
+        return num.ToString("#,0");
+    }
+}
