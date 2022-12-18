786af25577e57cc47291126de5d33e60eff5c13d
R1nge <r1ngegamedev@gmail.com>
2022-12-18 12:30:39 +0300

    Made modifiers constants

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -10,20 +10,24 @@ public class Data
     public float delay;
     public bool isUnlocked;
     public bool isAuto;
-    
+
+    private const int IncomeModifier = 2;
+    private const int LevelModifier = 10;
+    private const float TotalModifier = 1.25f;
+
     public event Action<float> OnWeaponUpgraded;
 
     public void Init()
     {
         weaponBaseIncome *= weaponLevel;
-        weaponPrice = (weaponBaseIncome * 2 + weaponLevel * 10) * 1.2f;
+        weaponPrice = (weaponBaseIncome * IncomeModifier + weaponLevel * LevelModifier) * TotalModifier;
     }
 
     public void UpgradeWeapon()
     {
         weaponLevel += 1;
         weaponBaseIncome *= weaponLevel;
-        weaponPrice = (weaponBaseIncome * 2 + weaponLevel * 10) * 1.25f;
+        weaponPrice = (weaponBaseIncome * IncomeModifier + weaponLevel * LevelModifier) * TotalModifier;
 
         if (weaponLevel >= 100)
         {
